1ffc2d09017819d1747abc6e361cb7e46f03773d
petuhovskiy <petuhovskiy@yandex.ru>
2018-05-05 08:39:16 +0300

    animation

--- src/Unfreeze.js
+++ src/Unfreeze.js
@@ -3,12 +3,20 @@ import Table from './Table';
 import LLTE from './LLTE';
 import cloneDeep from 'clone-deep';
 
+const ANIMATION_LENGTH = 1000;
+
 class Unfreeze extends Component {
     constructor(props) {
         super(props);
         this.state = {};
 
         this.onKeyDown = this.onKeyDown.bind(this);
+        this.animate = this.animate.bind(this);
+    }
+
+    animate() {
+        this.requestId = setTimeout(this.animate, 30);
+        this.setState({ time: Date.now() });
     }
 
     init() {
@@ -16,7 +24,7 @@ class Unfreeze extends Component {
         llte.reverse();
         this.setState({ 
             pending: llte,
-            table: {} 
+            table: {}
         });
         this.makeStep();
     }
@@ -24,10 +32,30 @@ class Unfreeze extends Component {
     componentWillMount() {
         this.init();
         document.addEventListener('keydown', this.onKeyDown);
+        if (!this.requestId) {
+            this.animate();
+        }
     }
 
     componentWillUnmount() {
         document.removeEventListener('keydown', this.onKeyDown);
+        if (this.requestId) {
+            clearTimeout(this.requestId);
+            this.requestId = null;
+        }
+    }
+
+    calcTransition(prevTable, _table) {
+        if (!prevTable || !prevTable.rows) {
+            return null;
+        }
+        const table = cloneDeep(_table);
+        table.rows.forEach(it => {
+            const row = LLTE.findRow(prevTable, it.id);
+            if (!row) return;
+            it.prevPlace = row.actualPlace;
+        });
+        return table;
     }
 
     makeStep() {
@@ -37,10 +65,14 @@ class Unfreeze extends Component {
             if (event) {
                 const table = cloneDeep(prevState.table);
                 LLTE.applyEvent(table, event);
+                
                 table.rows.sort(LLTE.placeComparator);
                 return {
                     pending,
-                    table
+                    table,
+                    composited: this.calcTransition(prevState.table, table),
+                    lastUpdate: Date.now(),
+                    time: Date.now()
                 }
             }
             return {};
@@ -60,7 +92,17 @@ class Unfreeze extends Component {
     }
 
     render() {
-        return <Table table={this.state.table} />;
+        const passed = this.state.time - this.state.lastUpdate;
+        let table;
+        if (this.state.composited && passed < ANIMATION_LENGTH) {
+            table = cloneDeep(this.state.composited);
+            table.rows.forEach(it => {
+                it.actualPlace = it.prevPlace + (it.actualPlace - it.prevPlace) * (passed / ANIMATION_LENGTH);
+            })
+        } else {
+            table = this.state.table;
+        }
+        return <Table table={table} />;
     }
 }
 
